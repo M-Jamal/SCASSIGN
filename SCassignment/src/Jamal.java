09c7eb551c36a3f00a2c135e46b1f430a376f7bf
Jamal <>
2016-12-19 21:49:36 +0500

    This is Jamal's final Build.

--- SCassignment/src/Jamal.java
+++ SCassignment/src/Jamal.java
@@ -1,20 +1,22 @@
+import java.util.Scanner;
+
+/////Muhammad Jamal Sabir = FA13-BSE-032
 
 public class Jamal {
 	private String user = "Admin";
 	private String pass = "12345";
-	private boolean authentication = false;
+	private String userclone = null;
+	private String passclone = null;
 	
-	public boolean getAuthentication(){
-		return authentication;
-	}
-	
-	public void Loginuser(String user,String pass){
+////////////////////////////////////////////////////////////////////////////////////////////////
+	public void Loginuser(String user,String pass,Jamal obj){
 		if(this.user.equals(user)){
 			if(this.pass.equals(pass)){
-				authentication = true;
+				this.userclone = user;
+				this.passclone = pass;
 				System.out.println("User Authorized.");
 				Adminoptions AO = new Adminoptions();
-				
+				AO.showmenu(obj);				
 			}
 			else{
 				System.out.println("Password is not Correct.");
@@ -24,5 +26,55 @@ public class Jamal {
 			System.out.println("User name invalid.");
 		}
 	}
+	
+	public String getAuthentication(){
+		return "Username: "+this.userclone+"\nPassword: "+this.passclone;
+	}
+	public boolean isloggedin(){
+		if(userclone.equalsIgnoreCase("") || userclone.equalsIgnoreCase(null)){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public void logout(){
+		this.userclone = "";
+		this.passclone = "";
+	}
+	
+//////////////////////////////////////////////////////////////////////////////////////////////
+	private Scanner read = new Scanner(System.in); 
+	private String passvarify = null;
+	protected boolean setuser(String changeuser){
+		System.out.println("Enter Current Password: ");
+		this.passvarify = read.nextLine();
+		if(this.passclone.equals(passvarify)){
+		this.userclone = changeuser;
+		this.passvarify = null;
+		return true;
+		}
+		else {
+			System.out.println("Provided Invalid Password.");
+			return false;
+		}
+	}
+	
+	protected boolean setpass(String changePass){
+		System.out.println("Enter Current Password: ");
+		this.passvarify = read.nextLine();
+		if(this.passclone.equals(passvarify)){
+		this.passclone = changePass;
+		this.passvarify = null;
+		return true;
+		}
+		else {
+			System.out.println("Provided Invalid Password.");
+			return false;
+		}
+	}
+/////////////////////////////////////////////////////////////////////////////////////////
+
 	
 }
